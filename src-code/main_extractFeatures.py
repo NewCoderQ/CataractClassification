bf76dd1dcd1b800d823325736d7a6665126f9601
funny_QZQ <qiaozhiqiang1993@163.com>
2017-09-11 16:48:17 +0800

    comment

--- src-code/main_extractFeatures.py
+++ src-code/main_extractFeatures.py
@@ -19,7 +19,7 @@ img_path = preProcess.getImgPath()		# the file path of train images
 # extractColorFeature.getGrayCount(img_pre_path, img_path, save_feature_path)
 
 # GLCM特征
-extractGLCMFeature.calcGLCMFeature(img_pre_path, img_path, save_feature_path)
+# extractGLCMFeature.calcGLCMFeature(img_pre_path, img_path, save_feature_path)
 
 # wave feature
 extractWaveFeature.calcWaveFeature(img_pre_path, img_path, save_feature_path)
