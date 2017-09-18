c0301645323fb96ba07cf70c5ed14cb23e8e075d
funny_QZQ <qiaozhiqiang1993@163.com>
2017-09-18 23:48:28 +0800

    update demo

--- src-code/normalization/cataract_demo.py
+++ src-code/normalization/cataract_demo.py
@@ -2,12 +2,14 @@
 # @Author: Zhiqiang
 # @Date:   2017-09-18 22:41:33
 # @Last Modified by:   funny_QZQ
-# @Last Modified time: 2017-09-18 23:38:20
+# @Last Modified time: 2017-09-18 23:48:05
 
 # import cv2
 import sys
 sys.path.append('../')
-import extractColorFeature
+import extractColorFeature		# color
+import extractGLCMFeature		# text-feature
+import extractWaveFeature		# wave
 
 def extract_feature(img_dir):
 	'''
@@ -23,7 +25,13 @@ def extract_feature(img_dir):
 	# 提示用户输入眼底图像的名字，不包含后缀名
 	img_name = raw_input("Please enter the name of the image for classifying(without the ext):")
 	img_name += '.jpg'
+	# 提取测试图像的颜色特征，并且将其保存在show_data文件夹下面的.pkl文件中
 	extractColorFeature.getGrayCount(img_dir, [img_name], 'show_data/')
+	extractGLCMFeature.calcGLCMFeature(img_dir, [img_name], 'show_data/')
+	extractWaveFeature.calcWaveFeature(img_dir, [img_name], 'show_data/')
+
+
+
 
 
 
