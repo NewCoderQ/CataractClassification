5cddde57c130021b22a0be720e97e9c218df221f
funny_QZQ <qiaozhiqiang1993@163.com>
2017-09-18 23:01:17 +0800

    update demo

--- src-code/normalization/cataract_demo.py
+++ src-code/normalization/cataract_demo.py
@@ -2,4 +2,30 @@
 # @Author: Zhiqiang
 # @Date:   2017-09-18 22:41:33
 # @Last Modified by:   Zhiqiang
-# @Last Modified time: 2017-09-18 22:43:06
+# @Last Modified time: 2017-09-18 23:00:55
+
+
+def extract_feature(img_path):
+	'''
+			根据用户输入的眼底图像的名称
+			来提取该图像的三个特征，并且保存在pkl文件中
+
+		Parameters:
+			img_path: 图像的文件目录
+
+		Returns:
+			None
+	'''
+	img_name = raw_input("Please enter the name of the image for classifying:")
+	img_path += img_name
+	print(img_path)
+
+
+
+
+
+if __name__ == '__main__':
+	extract_feature('../test/val/')
+	
+
+
