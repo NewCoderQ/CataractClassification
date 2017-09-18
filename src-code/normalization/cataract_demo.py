dbd91b130e6ad4d9485b2b82d2430daeea7ddef3
funny_QZQ <qiaozhiqiang1993@163.com>
2017-09-19 00:12:51 +0800

    update demo

--- src-code/normalization/cataract_demo.py
+++ src-code/normalization/cataract_demo.py
@@ -2,14 +2,30 @@
 # @Author: Zhiqiang
 # @Date:   2017-09-18 22:41:33
 # @Last Modified by:   funny_QZQ
-# @Last Modified time: 2017-09-18 23:51:15
+# @Last Modified time: 2017-09-19 00:11:52
 
 # import cv2
 import sys
 sys.path.append('../')
+import svmutil  				# import the libsvm model for svm
 import extractColorFeature		# color
 import extractGLCMFeature		# text-feature
 import extractWaveFeature		# wave
+import svmTrainGetWeight 		# train data process
+
+def train_model():
+	'''
+		train svm model:
+
+		Returns:
+			None
+	'''
+
+	color, GLCM, wave = svmTrainGetWeight.loadData()
+	svmTrainGetWeight.generateData_4(color, GLCM, wave, [0.13637379083	0.299172949594	6.65003458262])
+	print('model training...')
+	svmTrainGetWeight.svmTrain_4()
+
 
 def extract_feature(img_dir):
 	'''
@@ -32,13 +48,7 @@ def extract_feature(img_dir):
 	extractGLCMFeature.calcGLCMFeature(img_dir, [img_name], 'show_data/')
 	print('提取图像的小波特征：')
 	extractWaveFeature.calcWaveFeature(img_dir, [img_name], 'show_data/')
-
-
-
-
-
-
-
+	print('图像的特征提取完成！！！')
 
 
 
